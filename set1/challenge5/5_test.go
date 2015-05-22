1c2b6316efd92b075f1c22172edb9557d6b21bd1
George Czabania <george@czabania.com>
2015-05-22 18:01:19 +1200

    Cleanup challenge 5

--- set1/challenge5/5_test.go
+++ set1/challenge5/5_test.go
@@ -1,20 +1,23 @@
 package matasano
 
 import (
-	"encoding/hex"
-	"fmt"
+	"bytes"
 	"testing"
+
+	"github.com/stayradiated/matasano/enc/hex"
+	"github.com/stayradiated/matasano/xor"
 )
 
-func TestRepeatingKeyXor(t *testing.T) {
-	key := "ICE"
-	input := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-	output := hex.EncodeToString(RepeatingKeyXor([]byte(key), []byte(input)))
-	expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+func Test(t *testing.T) {
+	key := []byte("ICE")
+	plaintext := []byte("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
+	expected, _ := hex.Decode("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
 
-	if output != expected {
-		fmt.Println(output)
-		fmt.Println(expected)
-		t.Errorf("does not match expected output")
+	ciphertext := xor.Repeat(key, plaintext)
+
+	if bytes.Equal(ciphertext, expected) != true {
+		t.Log(ciphertext)
+		t.Log(expected)
+		t.Error("does not match expected output")
 	}
 }
